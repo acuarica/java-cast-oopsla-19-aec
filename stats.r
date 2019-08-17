37fbe29d6e459c44922de522e9068d926e84da76
Luigi <luismastrangelo@gmail.com>
2019-08-17 13:12:20 +0200

    Improved casts across projects plot, using boxplot+violin+jitter on outliers only.

--- stats.r
+++ stats.r
@@ -1,6 +1,7 @@
 library(RSQLite)
 library(reshape2)
 library(ggplot2)
+library(dplyr)
 
 dbReadObs <- function(dbPath) {
   print(sprintf("[Reading SQLite DB '%s']", dbPath))
@@ -19,15 +20,18 @@ stats.raw <- dbReadObs("stats-results/.output.sqlite3")
 stats.raw$value <- as.numeric(stats.raw$value)
 stats.project <- dcast(stats.raw, project~stat)
 stats.project$ratio <- stats.project$MethodWithCast/stats.project$Method
+stats.project.outliers <- stats.project[stats.project$Method>0,]
+stats.project.outliers <- stats.project.outliers %>% mutate(outlier = ratio > quantile(ratio,.75)+IQR(ratio)*1.5)
 
 pdf('stats-methodwcastXproject.pdf', height = 1.35)
-ggplot(stats.project, aes(x="", y=ratio))+
+ggplot(stats.project.outliers, aes(x="", y=ratio))+
+  geom_point(data = function(x) dplyr::filter_(x, ~ outlier), aes(size=CU,alpha=CU), position='jitter') +
+  geom_violin() +
   geom_boxplot(outlier.shape=NA)+
-  geom_jitter(aes(size=Method))+
   coord_flip()+
   theme_minimal()+
   theme(axis.title.y=element_blank(), axis.title.x=element_blank())+
-  labs(size="No. of Methods")
+  labs(size="No. of C.U.",alpha="No. of C.U.")
 dev.off()
 
 values <- c(
@@ -44,24 +48,24 @@ values <- c(
 writeDef('stats.def', values)
 
 
-qr.db = dbConnect(SQLite(), dbname="output.sqlite3")
-casts.table = dbReadTable(qr.db, "casts")
-repos.table = dbReadTable(qr.db, "repos")
-casts.with.link = casts.table[casts.table$link != '',]
-casts.no.link = casts.table[casts.table$link == '',]
-
-if (nrow(casts.table)!=nrow(casts.with.link)+nrow(casts.no.link)) stop("Casts with/without link mismatch")
-
-repos.with.casts = unique(casts.with.link$repoid)
-repos.no.link = unique(casts.no.link$repoid)
-repos.no.casts = setdiff(repos.table$repoid, repos.with.casts)
-
-repos.sum = dcast(casts.with.link, repoid~'nocasts', length)
-df <- merge(repos.table, repos.sum, by='repoid', all=TRUE)
-df$internalid <- NULL
-df$project <- paste(df$user, '/', df$repo, '@', df$host, sep='')
-df <- merge(stats.project, df, all=TRUE)
-df[is.na(df$Cast),]$Cast = 0
-df[is.na(df$nocasts),]$nocasts = 0
-df$diff <- df$nocasts - df$Cast
-sum(df$diff)
+# qr.db = dbConnect(SQLite(), dbname="output.sqlite3")
+# casts.table = dbReadTable(qr.db, "casts")
+# repos.table = dbReadTable(qr.db, "repos")
+# casts.with.link = casts.table[casts.table$link != '',]
+# casts.no.link = casts.table[casts.table$link == '',]
+# 
+# if (nrow(casts.table)!=nrow(casts.with.link)+nrow(casts.no.link)) stop("Casts with/without link mismatch")
+# 
+# repos.with.casts = unique(casts.with.link$repoid)
+# repos.no.link = unique(casts.no.link$repoid)
+# repos.no.casts = setdiff(repos.table$repoid, repos.with.casts)
+# 
+# repos.sum = dcast(casts.with.link, repoid~'nocasts', length)
+# df <- merge(repos.table, repos.sum, by='repoid', all=TRUE)
+# df$internalid <- NULL
+# df$project <- paste(df$user, '/', df$repo, '@', df$host, sep='')
+# df <- merge(stats.project, df, all=TRUE)
+# df[is.na(df$Cast),]$Cast = 0
+# df[is.na(df$nocasts),]$nocasts = 0
+# df$diff <- df$nocasts - df$Cast
+# sum(df$diff)
